9136618bd28039916df805e4ab29ee5bbb08fee8
briannovillo <brian@briannovillo.com.ar>
2019-03-13 16:54:34 -0300

    Add minimal implementation example

---
+++ minimal.js
@@ -0,0 +1,32 @@
+const Crawler = require("simplecrawler");
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const saveObjectIntoFile = (fileName, data) => fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
+
+const sites = [
+  "https://www.mercadolibre.com.ar",
+  "https://www.infobae.com"
+];
+
+sites.forEach(site => {
+  const crawler = new Crawler(site);
+
+  crawler.downloadUnsupported = false;
+  crawler.decodeResponses = true;
+
+  crawler.discoverResources = function(buffer, queueItem) {
+    const $ = cheerio.load(buffer.toString("utf8"));
+
+    return $("a[href]").map(function () {
+      return $(this).attr("href");
+    }).get();
+  };
+
+  crawler.on("fetchcomplete", function(queueItem, responseBuffer) {
+    console.log(queueItem.url);
+    saveObjectIntoFile("./scraped/"+new Date().getTime()+".html", responseBuffer);
+  });
+
+  crawler.start();
+});
